ed55b7fd303c6928165eca840d8a37c3a4eeb6a3
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-10-25 11:49:00 +0900

    [init] git ignore, dependence 세팅

---
+++ src/test/java/com/OEzoa/OEasy/OEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OEzoa.OEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
