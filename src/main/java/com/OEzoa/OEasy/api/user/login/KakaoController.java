919a748a87753b5415b7ea4f1e0dd49557ef6392
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-11-04 14:28:32 +0900

    [chore] 카카오 로그인 스웨거 적용

--- src/main/java/com/OEzoa/OEasy/api/user/login/KakaoController.java
+++ src/main/java/com/OEzoa/OEasy/api/user/login/KakaoController.java
@@ -3,6 +3,9 @@ package com.OEzoa.OEasy.api.user.login;
 import com.OEzoa.OEasy.application.user.UserService;
 import com.OEzoa.OEasy.application.user.dto.UserLoginResponseDTO;
 import com.OEzoa.OEasy.infra.api.KakaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Kakao Login API", description = "카카오 로그인 기능을 제공합니다.")
 @RequestMapping("kakao")
 public class KakaoController {
 
@@ -20,12 +24,16 @@ public class KakaoController {
     private final KakaoService kakaoService;
 
     @GetMapping("/callback")
+    @Operation(
+            summary = "카카오 API 로그인",
+            description = "카카오 API를 통해 사용자 정보를 받아 로그인을 처리합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "로그인 성공. JWT 토큰 반환."),
+                    @ApiResponse(responseCode = "400", description = "알 수 없는 오류 발생.")
+            }
+    )
+
     public ResponseEntity<UserLoginResponseDTO> kakaoCallback(@RequestParam("code") String code, HttpSession session) {
-
-//            // HttpServletResponse를 UserService로 전달하여 쿠키 설정을 처리
-//            UserLoginResponseDTO responseDTO = userService.loginWithKakao(code, response);
-//            return ResponseEntity.ok(responseDTO); // 로그인 성공 시 JWT 토큰 반환
-
         try {
             UserLoginResponseDTO responseDTO = userService.loginWithKakao(code, session);
             return ResponseEntity.ok(responseDTO); // 로그인 성공 시 JWT 토큰 반환
@@ -36,6 +44,13 @@ public class KakaoController {
 
     // 로그인 페이지 이동
     @GetMapping("/login")
+    @Operation(
+            summary = "카카오 로그인 페이지 이동",
+            description = "카카오 로그인 화면으로 리다이렉션됩니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공적으로 로그인 페이지로 이동.")
+            }
+    )
     public String redirectToKakao() {
         return "redirect:" + kakaoService.getKakaoLogin();
     }
--- src/main/java/com/OEzoa/OEasy/application/user/dto/UserLoginResponseDTO.java
+++ src/main/java/com/OEzoa/OEasy/application/user/dto/UserLoginResponseDTO.java
@@ -1,5 +1,6 @@
 package com.OEzoa.OEasy.application.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +9,10 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 public class UserLoginResponseDTO {
+    @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNiIsImlhdCI6MTczMDY4NTE5OCwiZXhwIjoxNzMwNjg4Nzk4fQ.Rq95AiUnHzqlPkEqLYQvlxWCon_3CUTrquqdmd1T-k0")
     private String accessToken;
+    @Schema(description = "아이디", example = "user123@naver.com")
     private String email;
+    @Schema(description = "닉네임", example = "user123@naver.com")
     private String nickname;
 }
