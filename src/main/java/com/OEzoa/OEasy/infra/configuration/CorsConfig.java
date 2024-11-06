a2d1534e438283e406f0349190f992d983960fcc
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-11-06 20:58:56 +0900

    [chore] cors 설정 추가

--- src/main/java/com/OEzoa/OEasy/infra/configuration/CorsConfig.java
+++ src/main/java/com/OEzoa/OEasy/infra/configuration/CorsConfig.java
@@ -3,16 +3,37 @@ package com.OEzoa.OEasy.infra.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
-
+@EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class CorsConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(corsConfiguration()));
+        http.httpBasic(HttpBasicConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(request -> request
+                .anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+
     @Bean
     public CorsConfigurationSource corsConfiguration() {
         CorsConfiguration config = new CorsConfiguration();
