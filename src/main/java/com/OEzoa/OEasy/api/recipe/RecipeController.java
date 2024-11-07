454413a4f8ddb2f0ffa980cfbac8f8a667c47fad
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2024-11-07 15:44:17 +0900

    [feat] 스웨거 주석 추가

--- src/main/java/com/OEzoa/OEasy/api/recipe/RecipeController.java
+++ src/main/java/com/OEzoa/OEasy/api/recipe/RecipeController.java
@@ -5,6 +5,7 @@ import com.OEzoa.OEasy.application.recipe.DTO.GetRecipeDTO;
 import com.OEzoa.OEasy.application.recipe.RecipeService;
 import com.OEzoa.OEasy.application.recipe.RecipeUploadService;
 import com.OEzoa.OEasy.application.recipe.RecipeValidator;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,15 @@ public class RecipeController {
     //private final RecipeUploadService recipeUploadService;
     private final RecipeService recipeService;
     private final RecipeValidator recipeValidator;
+
     @GetMapping("/{limit}")
+    @Operation(summary = "랜덤 이미지 불러오기", description = "limit에 해당하는 만큼 랜덤으로 불러옵니다.")
     public ResponseEntity<List<String>> getRandomImg(@PathVariable int limit) {
         return ResponseEntity.ok(recipeService.getRandomImg(limit));
     }
 
     @GetMapping()
+    @Operation(summary = "레시피 가져오기", description = "id에 해당하는 데이터를 불러옵니다.")
     public ResponseEntity<GetRecipeDTO> getRecipe(@RequestParam long id) {
         recipeValidator.isValidValue(id);
 
--- src/main/java/com/OEzoa/OEasy/application/recipe/DTO/GetRecipeDTO.java
+++ src/main/java/com/OEzoa/OEasy/application/recipe/DTO/GetRecipeDTO.java
@@ -1,5 +1,6 @@
 package com.OEzoa.OEasy.application.recipe.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,6 +11,10 @@ import java.util.List;
 @Getter
 public class GetRecipeDTO {
     private String recipeImg;
+    @Schema(description = "재료들 입니다.", example = "●오이무침 :\n" +
+            "오이 70g(1/3개), 다진 땅콩 10g(1큰술)\n" +
+            "●순두부사과 소스 : \n" +
+            "순두부 40g(1/8봉지), 사과 50g(1/3개)")
     private String ingredients;
     private String tip;
     private String title;
