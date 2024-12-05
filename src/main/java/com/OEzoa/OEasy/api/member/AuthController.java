7e07995ed0123f9ab1f2cf25fba26f4a9d31a352
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-12-05 12:14:59 +0900

    [chore] 리프레시 토큰 갱신 관련

--- src/main/java/com/OEzoa/OEasy/api/member/AuthController.java
+++ src/main/java/com/OEzoa/OEasy/api/member/AuthController.java
@@ -32,8 +32,9 @@ public class AuthController {
                     @ApiResponse(responseCode = "400", description = "잘못된 요청 또는 토큰 갱신 중 오류 발생.")
             }
     )
-    public ResponseEntity<AuthTokenResponseDTO> refreshAccessToken(@RequestHeader(name = "Authorization") String authorizationHeader) {
-        AuthTokenResponseDTO tokens = authService.refreshAccessToken(authorizationHeader);
+    public ResponseEntity<AuthTokenResponseDTO> refreshAccessToken(
+            @RequestHeader("Authorization") String refreshTokenHeader) {
+        AuthTokenResponseDTO tokens = authService.refreshAccessToken(refreshTokenHeader);
         return ResponseEntity.ok(tokens);
     }
 }
